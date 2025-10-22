c556d2b0cb871b14d8e64d5424134c539a49350f
SuperJingke <jingkeliang003@gmail.com>
2025-10-22 00:28:07 -0400

    update the react-scripts

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -11,17 +11,56 @@ const Works = () => {
       <div className="container">
         <div className="row">
           <div className="col-lg-4 grid-item">
-            <img className="img-fluid" src="/static/picture/1.jpg" alt="Wildlife" />
-            <h4>Wildlife</h4>
+            <img className="img-fluid" src="/static/picture/3.jpg" alt="Boat Trip" />
+            <h4>Boat Trip</h4>
           </div>
           <div className="col-lg-4 grid-item">
             <img className="img-fluid" src="/static/picture/2.jpg" alt="Snow Adventure" />
             <h4>Snow Adventure</h4>
           </div>
           <div className="col-lg-4 grid-item">
-            <img className="img-fluid" src="/static/picture/3.jpg" alt="Boat Trip" />
-            <h4>Boat Trip</h4>
+            <img className="img-fluid" src="/static/picture/12.jpg" alt="Lunch" />
+            <h4>Photography</h4>
           </div>
+          
+          <div className="col-lg-4 grid-item">
+            <img className="img-fluid" src="/static/picture/1.jpg" alt="Wildlife" />
+            <h4>Wildlife</h4>
+          </div>
+          <div className="col-lg-4 grid-item">
+            <img className="img-fluid" src="/static/picture/4.jpg" alt="Toronto" />
+            <h4>Photography</h4>
+          </div>
+         
+          <div className="col-lg-4 grid-item">
+            <img className="img-fluid" src="/static/picture/6.jpg" alt="Folk Music Band - flute" />
+            <h4>Music Band</h4>
+          </div>
+          <div className="col-lg-4 grid-item">
+            <img className="img-fluid" src="/static/picture/7.jpg" alt="Bike" />
+            <h4>Biking</h4>
+          </div>
+          <div className="col-lg-4 grid-item">
+            <img className="img-fluid" src="/static/picture/8.jpg" alt="Firework" />
+            <h4>Firework in Xiangyang</h4>
+          </div>
+          <div className="col-lg-4 grid-item">
+            <img className="img-fluid" src="/static/picture/9.jpg" alt="River-side" />
+            <h4>Riverside in Xiangyang</h4>
+          </div>
+          <div className="col-lg-4 grid-item">
+            <img className="img-fluid" src="/static/picture/10.jpg" alt="dad's bike" />
+            <h4>Biking in Hometown</h4>
+          </div>
+          <div className="col-lg-4 grid-item">
+            <img className="img-fluid" src="/static/picture/11.jpg" alt="Gondola Lift" />
+            <h4>Gondola Ride</h4>
+          </div>
+          <div className="col-lg-4 grid-item">
+            <img className="img-fluid" src="/static/picture/5.jpg" alt="Cherry Blossom" />
+            <h4>Under the Cherry Blossom</h4>
+          </div>
+          
         </div>
       </div>
     </section>
